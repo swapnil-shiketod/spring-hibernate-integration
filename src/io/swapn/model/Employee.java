0833947bfc18ee3e1b2834f252bb0ef495bc1d79
swapnil-shiketod <swapnilshiketod1111@gmail.com>
2020-02-23 09:06:30 +0530

    Project setup

---
+++ src/io/swapn/model/Employee.java
@@ -0,0 +1,34 @@
+package io.swapn.model;
+
+public class Employee {
+
+	private int eno;
+	private String address;
+	private String name;
+	private double salary;
+	public int getEno() {
+		return eno;
+	}
+	public void setEno(int eno) {
+		this.eno = eno;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	} 
+	 
+}
